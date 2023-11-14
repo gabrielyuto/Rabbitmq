7ea62cf044253db2b1165b3c75a34eba0ce4dad0
gabrielyuto <gabrielyuto2016@gmail.com>
2023-11-14 16:29:55 -0300

    add implementation

--- src/main/java/com/example/rabbitmq/producer/NotaFiscalProducer.java
+++ src/main/java/com/example/rabbitmq/producer/NotaFiscalProducer.java
@@ -1,4 +1,19 @@
 package com.example.rabbitmq.producer;
 
+import com.example.rabbitmq.domain.NotaFiscalRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import static com.example.rabbitmq.config.NotaFiscalExchangeConfig.EXCHANGE_NAME;
+import static com.example.rabbitmq.config.NotaFiscalRabbitConfig.ROUTING_KEY_NAME;
+
+@Component
+@RequiredArgsConstructor
 public class NotaFiscalProducer {
+  private final RabbitTemplate template;
+
+  public void sendMessage(NotaFiscalRequest request) {
+    template.convertAndSend(EXCHANGE_NAME, ROUTING_KEY_NAME, request);
+  }
 }
--- src/main/java/com/example/rabbitmq/service/NotaFiscalService.java
+++ src/main/java/com/example/rabbitmq/service/NotaFiscalService.java
@@ -1,9 +1,17 @@
 package com.example.rabbitmq.service;
 
 import com.example.rabbitmq.domain.NotaFiscalRequest;
+import com.example.rabbitmq.producer.NotaFiscalProducer;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class NotaFiscalService {
-  public void sendMessage(NotaFiscalRequest request){
+  private final NotaFiscalProducer producer;
 
+  public void sendMessage(NotaFiscalRequest request){
+    producer.sendMessage(request);
   }
 }
